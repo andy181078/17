965ca80f467af9e523100ae72f1f6ba4660fb5c8
andy181078 <andrbelov18@gmail.com>
2022-01-22 14:21:29 +0300

    2

---
+++ App.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
+/*import ProductsAll from "./components/ProductsAll"
+import goodsArr from "./goodsArr.json"*/
+
+import PagesRouter from './pages/PagesRouter';
+import PagesLinks from './pages/PagesLinks';
+
+ReactDOM.render( 
+  <BrowserRouter>
+    <div>
+      <PagesLinks />
+      <PagesRouter />
+    </div>
+  </BrowserRouter>
+, document.getElementById('container') );
+
+
+/*ReactDOM.render(
+    <ProductsAll goods={goodsArr} />
+    , document.getElementById('container')
+); */
+
+
+
